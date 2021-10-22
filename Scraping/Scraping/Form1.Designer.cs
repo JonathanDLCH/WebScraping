8dc3d7b7f87896da96d6991d6c90982868015412
JonathanDLCH <thefallensclasic@gmail.com>
2021-10-21 20:27:39 -0500

    Practica funcional

---
+++ Scraping/Scraping/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Scraping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1117, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1165, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1279, 12);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.webBrowser1.Location = new System.Drawing.Point(16, 55);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(757, 658);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(800, 468);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(539, 245);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 6000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser2.Location = new System.Drawing.Point(800, 55);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(539, 392);
+            this.webBrowser2.TabIndex = 5;
+            this.webBrowser2.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser2_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 729);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
+
---
+++ Scraping/Scraping/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Scraping
+{
+    public partial class Form1 : Form
+    {
+        String url = "https://www.sears.com.mx/resultados/q=reloj/";
+        int id = 110; //Numero de la pagina donde iniciara el scraping
+        int x = 1; //Indice para los productos
+        List<string> parts = new List<string>(); //arreglo donde almacenaremos nuestros productos
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            carga(); //metodo para cargar la pagina
+        }
+
+        public void carga()
+        {
+            this.webBrowser1.Url = new Uri(url + id); //Nos dezplazamos de pagina
+            textBox1.Text = url + id;
+            id++;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            timer1.Start(); //Inicia el timer cuando carga la pagina
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var links = webBrowser1.Document.GetElementsByTagName("article"); //Buscamos los elementos con el tag article (productos)
+            foreach (HtmlElement link in links)
+            {
+                parts.Add(link.InnerHtml.ToString()); //los agregamos a nuestro arreglo
+            }
+
+            timer1.Stop();
+            //MessageBox.Show("ok");
+            leer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            leer();
+        }
+
+        public void leer()
+        {
+            textBox1.Text = x.ToString();
+            if (x < parts.Count()) //si hay productos en el array
+            {
+                //richTextBox1.Text = parts.ElementAt(x);
+                webBrowser2.DocumentText = parts.ElementAt(x); //vamos consultando cada producto y lo colocamos en el webBrowser2
+                x++;
+            }
+            else
+            {
+                parts.Clear(); // limpiamos nuestro arreglo
+                x = 0;
+                carga(); //cargamos la siguiente pagina
+            }
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //richTextBox1.Text = "";
+            HtmlDocument doc = webBrowser2.Document;
+            //Debemos validar que hayproductos
+            if(webBrowser2.Document.Body.InnerText != null && webBrowser2.Document.Body.InnerText != "Aún no tienes productos en tu Bolsa.")
+            {
+                String nom = "";
+                String img = "";
+                String precioi = "";
+                String lista = "";
+                nom = webBrowser2.Document.GetElementsByTagName("p")[0].InnerHtml.ToString(); //Obtenemos el nombre del producto
+                try
+                {
+                    img = webBrowser2.Document.GetElementsByTagName("img")[0].GetAttribute("src"); //Obtenemos el link de la imagen
+                }
+                catch(Exception ex)
+                {
+
+                }
+                //Obtenemos los precios que tenga
+                if (webBrowser2.Document.GetElementsByTagName("span")[0].InnerHtml.ToString().Contains("Precio Internet:"))
+                {
+                    precioi = webBrowser2.Document.GetElementsByTagName("span")[1].InnerHtml.ToString();
+                }
+                else
+                {
+                    lista= webBrowser2.Document.GetElementsByTagName("span")[1].InnerHtml.ToString();
+                    try
+                    {
+                        precioi = webBrowser2.Document.GetElementsByTagName("span")[4].InnerHtml.ToString();
+                    }
+                    catch(Exception ex) {
+                        precioi = "0";
+                    }
+                }
+                richTextBox1.Text += nom + "\n";
+                richTextBox1.Text += img + "\n";
+                richTextBox1.Text += "internet: " +precioi + "\n";
+                richTextBox1.Text += "lista: " + lista + "\n";
+
+                insert(nom.Replace("'", ""), img,lista.Replace("$","").Replace(",", ""), precioi.Replace("$", "").Replace(",", "")); //Realizamos la insercion en nuestra DB
+            }
+            leer();
+
+        }
+
+        private void insert(String nombre,String img,String lista,String internet)
+        {
+            String connectionline = "datasource=127.0.0.1; port=3306;username=root;password=;database=sears2;"; //127...Direccion del localhost
+            MySqlConnection conn = new MySqlConnection(connectionline); //Creamos nuestra conexion con la base de datos.
+
+            String sql = "insert into articulo values('"+nombre+"','"+img+"','"+lista+"','"+internet+"')"; //llamamos nuestra consulta
+
+            MySqlCommand command = new MySqlCommand(sql,conn);
+
+            command.Connection.Open();
+            command.ExecuteNonQuery(); //Corre la consulta
+
+
+            conn.Close();
+        }
+    }
+}
